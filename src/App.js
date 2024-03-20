5a0ea3c2cecc8001afdf71c282a33c67bceccb4f
kunjirviraj0321@gmail.com <kunjirviraj0321@gmail.com>
2024-03-20 16:22:04 +0530

    Few change in route

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React from 'react'
-import Navbar from './components/common/Navbar'
-import { Route, Routes } from 'react-router-dom'
-import HomePage from './pages/HomePage'
+import React from "react";
+import Navbar from "./components/common/Navbar";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 const App = () => {
   return (
-    <div className='w-screen min-h-screen bg-main flex flex-col font-inter'>
+    <div className="w-screen min-h-screen bg-main flex flex-col font-inter">
       <Navbar />
 
       <Routes>
-        <Route path='/' element={<HomePage />} />
+        <Route path="/" element={<HomePage />} />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
