9d9c7e0ae66b9c425980c257abc5f5c4cd4e688a
Pranay Bhandrkar <pranay.22110947@viit.ac.in>
2024-03-22 10:16:16 +0530

    Change in Home Page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>
-      Home Page
+    <div className='text-2xl text-white font-bold'>
+      This is a Home Page, It is in Building stage
     </div>
   )
 }
